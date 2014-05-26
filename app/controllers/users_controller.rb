790a7eeb6c2797fee6d6ba1d1be527166dbcbc88
Huaming Rao <huaming.rao@gmail.com>
2014-05-26 16:27:14 -0500

    defind users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
       format.html { render }
     end
   end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    respond_to do |format|
+      if @user.save
+        format.json { render json: { name: @user.name, email: @user.email } }
+      else
+        format.json { render json: { errors: @user.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,45 @@
 require 'spec_helper'
 
 describe UsersController do
+  before do
+    @user = FactoryGirl.build(:user)
+    expect(@user).to be_valid
+  end
 
   describe '#new' do
     it 'should response success' do
       get :new
       expect(response).to be_success
     end
+  end
+
+  describe '#create' do
+    subject(:data) do
+      {
+        name: @user.name,
+        email: @user.email,
+        password: @user.password,
+        format: :json
+      }
+    end
+
+    context 'provide correct information' do
+      it 'should return the created user as json' do
+        expect{ post :create, data }.to change { User.count }.by 1
+      end
+    end
+
+    context 'provide incorrect information' do
+      it 'should return error messages' do
+        @user.name = ''
+        expect{ post :create, data }.not_to change { User.count }.by 1
+        expect(response.body.to_json).to include("name")
+        @user.password = '12345'
+        expect{ post :create, data }.not_to change { User.count }.by 1
+        expect(response.body.to_json).to include("password")
+      end
+    end
+
   end
 
 end
